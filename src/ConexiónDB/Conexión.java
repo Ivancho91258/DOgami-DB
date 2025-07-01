7bfcbb377424430d7c4a328090598b9ae0b23eb4
Iván Mauricio Molina Pérez <ivancho91258@gmail.com>
2025-06-30 22:56:24 -0500

    Código de conexión a postgreSQL

---
+++ src/ConexiónDB/Conexión.java
@@ -0,0 +1,63 @@
+package ConexiónDB;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class Conexión {
+	private static final String URL= "jdbc:postgresql://localhost:5432/DOgami";
+	private static final String USUARIO_DB = "IvanMolina";
+	private static final String CONTRASENA_DB = "Maorintak1.";
+	
+	public static void main(String[] args) {
+		Connection conexion = null;
+		Statement sentencia = null;
+		ResultSet resultado = null;
+		
+		try {
+			
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Driver JDBC de PostgreSQL cargado correctamente");
+			
+			conexion = DriverManager.getConnection(URL, USUARIO_DB, CONTRASENA_DB);
+			System.out.println("Conexión exitosa a PostgreSQL");
+			
+			sentencia = conexion.createStatement();
+			
+			String sqlCreateTable = "CREATE TABLE IF NOT EXISTS personas (" +
+									"id SERIAL PRIMARY KEY," +
+									"nombre VARCHAR(45) NOT NULL," +
+									"email VARCHAR(25)," +
+									"contrasena_hash VARCHAR(20) NOT NULL," +
+									")";
+			sentencia.executeUpdate(sqlCreateTable);
+			System.out.println("Tabla 'personas' creada (o ya existía).");
+			
+			/* Ejemplo de consulta de datos */
+			
+			String sqlSelect = "SELECT id, nombre, email, contrasena_hash FROM usuario";
+			resultado = sentencia.executeQuery(sqlSelect);
+			
+			System.out.println("Datos en la tabla 'usuario':");
+			if (!resultado.isBeforeFirst()){
+				System.out.println("No hay usuarios registrados en la tabla 'usuario'");
+			} else {
+				while (resultado.next()) {
+					int id = resultado.getInt("id");
+                    String nombre = resultado.getString("nombre");
+                    String email = resultado.getString("email");
+                    String contrasenaHash = resultado.getString("contrasena_hash");
+                    
+                    System.out.println("ID: "+ id + ", Nombre: " + nombre + ", Email: " + email + ", Contraseña: " + contrasenaHash);
+					}
+				
+				}
+		}
+	}
+
+	private static String hashPassword(String rawPassword1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
