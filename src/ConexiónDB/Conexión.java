c08e1f53116279f161a6dca7ab610795a55aafdd
Iván Mauricio Molina Pérez <ivancho91258@gmail.com>
2025-07-03 20:30:33 -0500

    Consulta de datos

--- src/ConexiónDB/Conexión.java
+++ src/ConexiónDB/Conexión.java
@@ -23,8 +23,24 @@ public class Conexión {
 			// Se realiza la conexión con la base de datos
 			
 			conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
-			System.out.println("Conexión exitosa");
-			
+			System.out.println("Conexión exitosa con la DB");
+
+			//Consulta de todos los datos de la tabla
+			System.out.println("Consultando tabla: ");
+			String sqlSelectAll = "SELECT id, nombre, correo, contraseña, tipo_de_licencia FROM usuario";
+			try (Statement stmt = conexion.createStatement();
+					ResultSet rs = stmt.executeQuery (sqlSelectAll)){
+				while (rs.next()) {
+					int id = rs.getInt ("id");
+					String nombre = rs.getString ("nombre");
+					String correo = rs.getString ("correo");
+					String contraseña = rs.getString ("contraseña");
+					String tipo_de_licencia = rs.getString ("tipo_de_licencia");
+					System.out.println("ID: "+id+",\nNombre: "+nombre+",\nCorreo: "+correo+"\nContraseña: "+contraseña+",\nTipo de licencia: "+tipo_de_licencia);
+				}
+			} catch (SQLException e) {
+				System.err.println("Error al ejecutar la consulta: "+e.getLocalizedMessage());
+			}
 		} catch (ClassNotFoundException e) {
 			
 			System.err.print("Error: El driver no se cargo correctamente");
@@ -33,6 +49,6 @@ public class Conexión {
 			System.err.print("Error de conexión con la base de datos, revisa tus credenciales");
 			e.printStackTrace();
 		}
-	}
+	} 
 }
 			
